d3df656a499fc9b8db8bfb7fdadf5ab025a8e28b
tbagwill <tylerray.bagwill@calbaptist.edu>
2022-10-04 11:49:30 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+/* 
+ * Integer Linear Programming
+ * CSC 414 Midterm Project
+ * Tyler Bagwill, Nathan Bonetto, Kaeleb Brandin, Judah Lopez 
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        
+
+    }
+}
